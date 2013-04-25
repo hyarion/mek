f37f5b8bfcb30118667078be0b5299ec33490453
Benjamin Eriksson <benjamin.eriksson@gmail.com>
2013-04-25 14:37:27 +0200

    Adds debug variables and fixes some minor bugs

--- calculateAccelerations.m
+++ calculateAccelerations.m
@@ -1,16 +1,23 @@
-function acelerations = calculateAccelerations(positions, masses, gravitationalConstant)
+function accelerations = calculateAccelerations(positions, masses, gravitationalConstant)
 	[numberOfBodies,numberOfDimensions] = size(positions);
 
 	differenceVectors = calculateDifferenceVectors(positions);
 	distanceVectors = differenceVectors.^2; % rename difference per axis squared
-	distanceCube = sqrt(sum(distanceVectors, 3)).^3;
+	distances = sqrt(sum(distanceVectors, 3));
+	distances += eye(numberOfBodies); % enable us to divide by distance
+
+	distanceCube = distances.^3;
 	
-	distanceCube += eye(numberOfBodies);
+	%distanceCube += eye(numberOfBodies);
 
 	accelerations = zeros(numberOfBodies,numberOfDimensions);
 	for dimension = 1:numberOfDimensions
 		tmp1 = gravitationalConstant * masses;
 		tmp2 = differenceVectors(:,:,dimension);
-		acelerations(:,dimension) = tmp1 * (tmp2 ./ distanceCube);
+		accelerations(:,dimension) = tmp1 * (tmp2 ./ distanceCube);
 	end
 
+	global potentialEnergy = zeros(numberOfBodies,1);
+	massMatrix = masses' * masses;
+	massMatrix(logical(eye(numberOfBodies))) = 0;
+	potentialEnergy = sum(gravitationalConstant * (massMatrix ./ distances),1);
--- orbit_1body.m
+++ orbit_1body.m
@@ -1,5 +1,5 @@
-function [position,velocity,t]=orbit_1body(G,M,p0,v0,dt,tmax)
-	masses = [M 1];
+function [position,velocity,t]=orbit_1body(G,M,m,p0,v0,dt,tmax)
+	masses = [M m];
 	gravitationalConstant = G;
 
 	%gravitationalConstant = 6.67384E-11;
@@ -10,12 +10,20 @@ function [position,velocity,t]=orbit_1body(G,M,p0,v0,dt,tmax)
 	velocity  = zeros(numberOfSteps,2);
 	sateliteAcceleration = zeros(numberOfSteps,2);
 
+	% Debug
+	global kineticEnergyVector = zeros(numberOfSteps,1);
+
 	position(1,:) = p0;
 	velocity(1,:) = v0;
 
 	positions = [0 0 ; p0];
 	tmp = calculateAccelerations(positions, masses, gravitationalConstant);
 	sateliteAcceleration(1,:) = tmp(2,:);
+
+	global potentialEnergy;
+	global potentialEnergyVector;
+	potentialEnergyVector = zeros(numberOfSteps,1);
+	size(potentialEnergyVector)
 
 	for s = 1:numberOfSteps-1
 		t(s+1) = t(s) + dt;
@@ -25,6 +33,9 @@ function [position,velocity,t]=orbit_1body(G,M,p0,v0,dt,tmax)
 		sateliteAcceleration(s,:) = accelerations(2,:);
 		position(s+1,:) = position(s,:) + velocity(s,:)*dt + 0.5 * sateliteAcceleration(s,:) * (dt^2);
 		velocity(s+1,:) = velocity(s,:) + 0.5 * (sateliteAcceleration(s+1,:) + sateliteAcceleration(s,:)) * dt;
+
+		potentialEnergyVector(s+1) = 0.5*sum(potentialEnergy);
 	end
 	
-
+	kineticEnergyVector = sum(velocity.^2, 2) .* (m/2);
+	size(kineticEnergyVector)
