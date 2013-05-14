08c9cb14e820e3e23b75ab5ae96f878a6d9532f8
Emil Eriksson <emil.eriksson@codemill.se>
2013-05-14 23:21:48 +0200

    Uppg4

--- uppg4.m
+++ uppg4.m
@@ -1,16 +1,36 @@
 G = 6.67384E-11;
 M = 5.972E24;
-m = 450E3;
-masses = [M m];
-p0 = [0 0;412E3+6.371E6 0];
-v0 = [0 0;0 7706.6];
+masses = [
+	1.989E30
+	5.5E-2 * M
+	8.15E-1 * M
+	1 * M
+	1.07E-1 * M
+]';
+Au = 1.49597871E11;
+p0 = [
+	0 0
+	0 4E-1 * Au
+	0 7E-1 * Au
+	0 1 * Au
+	0 1.5 * Au
+];
+v0 = [
+	0 0
+	4.787E4 0
+	3.502E4 0
+	2.978E4 0
+	2.4077E4 0
+];
 
-dt = 10;
-tmax = 1E4;
+dt = 1E3;
+tmax = 60*60*24*365.25 / 2;
 
 hold off;
 
 [p,v,t] = orbit_Nbody(G, masses, p0,v0, dt,tmax);
-size(p)
-plot(p(1,1,:),p(1,2,:),p(2,1,:),p(2,2,:))
-
+%size(p)
+xs = rotdim(p(:,1,:));
+ys = rotdim(p(:,2,:));
+plot( xs,ys );
+legend('Solen','Merkurius','Venus','Jorden','Mars');
