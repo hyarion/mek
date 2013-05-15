09fb4650774ad02d7a399169ba93d965184d7070
Emil Eriksson <emil.eriksson@codemill.se>
2013-05-15 23:30:55 +0200

    Calculates orbital times

--- uppg4.m
+++ uppg4.m
@@ -9,29 +9,25 @@ masses = [
 ]';
 Au = 1.49597871E11;
 p0 = [
-	0 0
-	0 4E-1 * Au
-	0 7E-1 * Au
-	0 1 * Au
-	0 1.5 * Au
+	0, 0
+	4E-1 * Au, 0
+	7E-1 * Au, 0
+	1 * Au, 0
+	1.5 * Au, 0
 ];
 v0 = [
 	0 0
-	4.787E4 0
-	3.502E4 0
-	2.978E4 0
-	2.4077E4 0
+	0 4.787E4
+	0 3.502E4
+	0 2.978E4
+	0 2.4077E4
 ];
-%v0
-%initialMomentum = sum(masses'.*v0(:,1))
 sunspeed = [-sum(masses' .* v0(:,1)),-sum(masses' .* v0(:,2))] ./ masses(1);
 v0(1,:) = sunspeed;
-%initialMomentum = sum(masses'.*v0(:,1))
 v0(2:5,:) += ones(4,1) * sunspeed;
-%initialMomentum = sum(masses'.*v0(:,1))
 
-dt = 1E3;
-tmax = 60*60*24*365.25;
+dt = 1E4;
+tmax = 60*60*24*365.25 * 2;
 
 hold off;
 
@@ -50,26 +46,39 @@ totalMomentum = (sum(momentum,1));
 xs = rotdim(totalMomentum(1,1,:));
 ys = rotdim(totalMomentum(1,2,:));
 plot(t,sqrt(xs.^2+ys.^2))
+ylabel('Linear momentum (kg m^2/s)')
+xlabel('Time (s)')
 print(gcf, '-dpng', 'uppg4_momentum.png');
+clear momentum
 
 printf('Kinetic energy\n')
 kinetic = calculateKineticEnergy(masses', v);
 totalKinetic = sum(kinetic,1);
 plot(t,kinetic,t,totalKinetic)
-legend('Solen','Merkurius','Venus','Jorden','Mars');
+ylabel('Kinetic Energy (J)')
+xlabel('Time (s)')
+legend('Solen','Merkurius','Venus','Jorden','Mars','Total');
 print(gcf, '-dpng', 'uppg4_kinetic.png');
+
 
 printf('Potential energy\n')
 potential = calculatePotentialEnergy(masses', p, G);
 plot(t',potential,t',sum(potential,1))
+ylabel('Potential Energy (J)')
+xlabel('Time (s)')
 legend('Solen','Merkurius','Venus','Jorden','Mars','Total');
 %potential
 print(gcf, '-dpng', 'uppg4_potential.png');
 
 printf('Total energy\n')
-plot(t, potential + kinetic);
-legend('Solen','Merkurius','Venus','Jorden','Mars');
+plot(t, potential + kinetic, t, sum(potential + kinetic));
+ylabel('Energy (J)')
+xlabel('Time (s)')
+legend('Solen','Merkurius','Venus','Jorden','Mars','Total');
 print(gcf, '-dpng', 'uppg4_total_energy.png');
+clear kinetic
+clear totalKinetic
+clear potential
 
 printf('Suns position\n')
 xs = rotdim(p(1,1,:));
@@ -78,7 +87,22 @@ plot(xs,ys);
 print(gcf, '-dpng', 'uppg4_sun_position.png');
 
 printf('Orbital times\n')
-printf('\tVenus\n')
+printf('\tMerkurius: ')
 xs = rotdim(p(2,1,:));
 ys = rotdim(p(2,2,:));
-printf('\t\t%g\n',orbit_time(xs,ys,t))
+printf('%g\n',orbit_time(xs,ys,t))
+
+printf('\tVenus: ')
+xs = rotdim(p(3,1,:));
+ys = rotdim(p(3,2,:));
+printf('%g\n',orbit_time(xs,ys,t))
+
+printf('\tJorden: ')
+xs = rotdim(p(4,1,:));
+ys = rotdim(p(4,2,:));
+printf('%g\n',orbit_time(xs,ys,t))
+
+printf('\tMars: ')
+xs = rotdim(p(5,1,:));
+ys = rotdim(p(5,2,:));
+printf('%g\n',orbit_time(xs,ys,t))
